356225f4a50d34e511611976c45ac9db5a0a0b2a
jewel-etc <jewel.etc@gmail.com>
2023-01-25 09:49:14 +0530

    deploy

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use('/api/quesAns', quesAnsRoutes)
 app.use((req, res, next) => {
 
     const error = new HttpError('Could not find this route', 404);
+    console.log(error)
     return next(error);
 
 })
@@ -67,6 +68,7 @@ app.use((error, req, res, next) => {
 })
 
 mongoose
+    // .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zze4e.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
     .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zze4e.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
     .then(() => {
         console.log("Connect to Database")
--- models/topic-schema.js
+++ models/topic-schema.js
@@ -1,14 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const topicSchema = new Schema({
-    name: { type: String, required: true },
-    description: { type: String},
-    // subjectId: { type: mongoose.Types.ObjectId, required: true, ref: 'Subject' },
-    quesAnsIds:[{ type: mongoose.Types.ObjectId, required: true, ref: 'QuesAns' }]
+  name: { type: String, required: true },
+  description: { type: String },
+  quesAnsIds: [
+    { type: mongoose.Types.ObjectId, required: true, ref: "QuesAns" },
+  ],
+});
 
-
-})
-
-module.exports = mongoose.model('Topic', topicSchema);
+module.exports = mongoose.model("Topic", topicSchema);
